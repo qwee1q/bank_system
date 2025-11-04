e0237200c5dcdbeb82d0fce2669d8aac23f06c1a
qwee1q <qweeq8341@gmail.com>
2025-11-04 15:09:24 +0200

    add simple func and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register',[RegisterController::class,'create'])->name('registration');
+
+Route::post('/register',[RegisterController::class,'store'])->name('registration');
+
+Route::get('/login',[LoginController::class,'create'])->name('login');
+
+Route::post('/login',[LoginController::class,'store'])->name('login');
